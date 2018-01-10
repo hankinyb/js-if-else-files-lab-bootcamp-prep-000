8d2c44a9b06544a052960879b7c49be962291163
Yan Hankin <hankinyb@gmail.com>
2018-01-10 21:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("The name is correct")
+ } else { console.log("Expected the name to be Susan")
  }
-else { ("Expected the name to be Susan")}
 
 
 
